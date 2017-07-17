b18b855324788c5c17b8b1bc03d4fd4368483eb7
tenglai <13049322087@163.com>
2017-07-17 23:42:54 +0800

    $ git remote add origin git@github.com:tenglai/douban.git

---
+++ src/components/Bookshelf.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Bookshelf',
+    data() {
+      return {
+        msg: '书架'
+      }
+    }
+  }
+</script>
+
+<style scoped></style>
---
+++ src/components/Rank.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>男生</p>
+    <ul class="rank-type">
+      <li v-for="item in ranklist.male" v-if="!item.collapse" :key="item._id">
+        <RankItem :rankInfo="item"></RankItem>
+      </li>
+      <li class="other-rank" @click="showMoreMaleBank">
+        <span>
+          <Icon name="bar-chart"></Icon>
+          别人家的排行榜
+        </span>
+        <span class="angle">
+          <Icon v-if="maleOtherRankIsShow" name="angle-up"></Icon>
+          <Icon v-else name="angle-down"></Icon>
+        </span>
+      </li>
+      <ul v-show="maleOtherRankIsShow" class="rank-type">
+        <li v-for="item in ranklist.male" v-if="item.collapse" :key="item._id">
+          <RankItem :rankInfo="item"></RankItem>
+        </li>
+      </ul>
+    </ul>
+    <p>女生</p>
+    <ul class="rank-type">
+      <li v-for="item in ranklist.female" v-if="!item.collapse" :key="item._id">
+        <RankItem :rankInfo="item"></RankItem>
+      </li>
+      <li class="other-rank" @click="showMoreFemaleRank">
+        <span>
+          <Icon name="bar-chart"></Icon>
+          别人家的排行榜
+        </span>
+        <span class="angle">
+          <Icon v-if="femaleOtherRankIsShow" name="angle-up"></Icon>
+          <Icon v-else name="angle-down"></Icon>
+        </span>
+      </li>
+      <ul v-show="femaleOtherRankIsShow" class="rank-type">
+        <li v-for="item in ranklist.female" v-if="item.collapse" :key="item._id">
+          <RankItem></RankItem>
+        </li>
+      </ul>
+    </ul>
+  </div>
+</template>
