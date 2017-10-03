432716ebb0259f4017afdbe4939631694e9ec8f3
tenglai <13049322087@163.com>
2017-10-03 15:43:41 +0800

    loading component

--- news/src/components/loading/index.js
+++ news/src/components/loading/index.js
@@ -1,8 +1,8 @@
 const LoadingComponent = require('./Loading.vue')
 const loading = {
   install: function(Vue) {
+  	// 注册组件
     Vue.component('loading', LoadingComponent)
   }
 }
 module.exports = loading;
-
