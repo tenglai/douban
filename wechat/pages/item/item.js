bd74bb3cbb5b5df922f686040ad4dbe29709236b
tenglai <13049322087@163.com>
2018-01-12 18:27:44 +0800

    add item

--- wechat/pages/item/item.js
+++ wechat/pages/item/item.js
@@ -5,62 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    title: '',
+    loading: true,
+    movie: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    const _this = this;
+    // 拼接请求url
+    const url = 'https://api.douban.com/v2/movie/subject/' + options.id;
+    // 请求数据
+    wx.request({
+      url: url,
+      data: {},
+      header: {
+        'content-type': 'json' // 默认值
+      },
+      success:function(res) {
+        // 赋值
+        _this.setData({
+          movie: res.data,
+          loading: false // 隐藏等待框
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    // 修改导航栏标题
+    wx.setNavigationBarTitle({
+      title: this.data.title + '<<电影<<豆瓣'
+    })
   }
 })
