042677112530fd4b2bb925d7027339f680294005
tenglai <13049322087@163.com>
2018-01-17 23:52:40 +0800

    add sidebar

--- school/pages/audiobook/audiobook.js
+++ school/pages/audiobook/audiobook.js
@@ -1,66 +1,37 @@
 // pages/audiobook/audiobook.js
+var data_ = require('../../utils/data.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    navLeftItems: [],
+    navRightItems: [],
+    curNav: 1,
+    curIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function () {
+    this.setData({
+      navLeftItems: data_.getSData(),
+      navRightItems: data_.getSData()
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  //事件处理函数  
+  switchRightTab: function (e) {
+    // 获取item项的id，和数组的下标值  
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    // 把点击到的某一项，设为当前index  
+    this.setData({
+      curNav: id,
+      curIndex: index
+    })
   }
 })
